50eccdd2ea374e7a8778ed397d49fd707455a1fc
Berkin Kırbıyık <berkinkirbiyik@tutanota.com>
2024-02-07 00:06:15 +0300

    Update darklight.js

--- assets/js/darklight.js
+++ assets/js/darklight.js
@@ -1,6 +1,6 @@
 // assets/js/script.js
 
-document.getElementById('toggleButton').addEventListener('click', function() {
+document.getElementById('Aydınlık/Karanlık mod').addEventListener('click', function() {
   document.body.classList.toggle('dark-mode');
 });
 
